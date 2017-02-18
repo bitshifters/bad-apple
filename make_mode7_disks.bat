58839802388d1a1774dd919603129580107de3f6
kieranhj <kieran@kconnell.co.uk>
2017-02-18 19:16:43 +0000

    Proper music!
    First version using Inverse Phase's proper music although still not
    quite finished.
    Removed command line code as we don't need it.
    Moved decrunch tables to OS memory.
    Moved main code down to &1100.
    Changed streaming ring buffer to 3 tracks - still works (fingers
    crossed)
    Getting there...

--- make_mode7_disks.bat
+++ make_mode7_disks.bat
@@ -5,11 +5,12 @@ del "%1\disks\%1_disk2.ssd"
 bin\bbcim -a "%1\disks\%1_disk1.ssd" "m7vplay"
 CD files
 ..\bin\bbcim -a "..\%1\disks\%1_disk1.ssd" "Boot"
-..\bin\bbcim -a "..\%1\disks\%1_disk1.ssd" "Dummy4"
+..\bin\bbcim -a "..\%1\disks\%1_disk1.ssd" "Dummy2"
 ..\bin\bbcim -a "..\%1\disks\%1_disk2.ssd" "Dummy8"
 CD "..\%1\files"
 ..\..\bin\bbcim -a "..\disks\%1_disk1.ssd" "%1_beeb_00"
 ..\..\bin\bbcim -a "..\disks\%1_disk2.ssd" "%1_beeb_01"
+..\..\bin\bbcim -a "..\disks\%1_disk2.ssd" "%1_beeb_02"
 CD "..\disks"
 ..\..\bin\bbcim -interss sd %1_disk1.ssd %1_disk2.ssd %1.dsd
 CD ..\..
