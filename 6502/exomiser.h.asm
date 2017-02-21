9a629c2bf01e4631302414ff1424f0c51ab938f3
SimonM <podvilla@gmail.com>
2017-02-21 22:03:43 +0000

    Added final music

--- 6502/exomiser.h.asm
+++ 6502/exomiser.h.asm
@@ -15,13 +15,21 @@
 \ *	Space reserved for runtime buffers not preinitialised
 \ ******************************************************************
 
-MUS_buffer_len = 1024			; this is now packed in to language workspace at &0400
-\\ If you want to make this bigger than 1024 then need to find somewhere else to put it!!
+; if music was compiled with 2Kb compression window, we must use different buffer settings
+IF _MUS_BUFFER_2K
+    MUS_buffer_len = 2048
+    ; MUS_buffer_start and MUS_buffer_end MUST be declared somewhere in code.
+ELSE
 
-\\ Exomiser unpack buffer (must be page aligned)
-\\ Now moved this to the language workspace at &0400 - &0800
-MUS_buffer_start = &400
-MUS_buffer_end = MUS_buffer_start + MUS_buffer_len
+    MUS_buffer_len = 1024			; this is now packed in to language workspace at &0400
+    \\ If you want to make this bigger than 1024 then need to find somewhere else to put it!!
+
+    \\ Exomiser unpack buffer (must be page aligned)
+    \\ Now moved this to the language workspace at &0400 - &0800
+    MUS_buffer_start = &400
+    MUS_buffer_end = MUS_buffer_start + MUS_buffer_len
+ENDIF
+
 
 ; -------------------------------------------------------------------
 ; this 156 byte table area may be relocated. It may also be clobbered
