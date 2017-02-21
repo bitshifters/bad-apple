76f26fcf8dfcfc4030db751f320b236219954512
SimonM <podvilla@gmail.com>
2017-02-21 23:43:23 +0000

    Tweaked disk layout
    First 7 tracks now free (gives us 2560 bytes extra exe size to play
    with)

--- compress_mode7_stream.bat
+++ compress_mode7_stream.bat
@@ -2,5 +2,12 @@ REM Usage: compress_mode7_stream <short name/dir>
 @echo off
 DEL /Q %1\files\*
 bin\exomizer.exe raw -m 3072 -c %1\%1_beeb.bin -o %1\%1_beeb.bin.exo
-bin\split.exe --byte=189440 --numeric-suffixes %1\%1_beeb.bin.exo %1\files\%1_beeb_
+rem 186880 is the first 73 tracks worth of video (2560 x 73)
+rem first 7 tracks of the disk are reserved
+bin\split.exe --byte=186880 --numeric-suffixes %1\%1_beeb.bin.exo %1\files\%1_beeb_
+
+rem or first 6 tracks variant
+rem bin\split.exe --byte=189440 --numeric-suffixes %1\%1_beeb.bin.exo %1\files\%1_beeb_
+
+
 FOR %%F IN (%1\files\*) DO echo $.%1	000000 000000 > %%F.inf
